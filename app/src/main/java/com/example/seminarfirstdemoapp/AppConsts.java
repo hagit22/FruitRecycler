2cdd5bf4bc3ce597b6785df43af75d5130ba0b52
AllTeach <33581448+AllTeach@users.noreply.github.com>
2025-06-16 18:26:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/seminarfirstdemoapp/AppConsts.java
@@ -0,0 +1,10 @@
+package com.example.seminarfirstdemoapp;
+
+public class AppConsts {
+
+    public static final int ROWS = 3;
+    public static final int COLUMNS = 3;
+    public static final int EMPTY = 0;
+    public static final int PLAYER_X = 1;
+    public static final int PLAYER_O = -1;
+}
---
+++ app/src/main/java/com/example/seminarfirstdemoapp/ExampleActivity.java
@@ -0,0 +1,58 @@
+package com.example.seminarfirstdemoapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ExampleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_example);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+      //  checkIfFirstRun();
+
+
+
+    }
+
+    public void moveActivities(View view) {
+        // read the data from the edit text
+        // pass the data to the next activity
+        EditText editText =findViewById(R.id.etNameExample);
+        String name = editText.getText().toString();
+
+        checkToSaveName(name);
+
+
+        Intent intent =  new Intent(this, MainActivity.class);
+        intent.putExtra(getString(R.string.user_name), name);
+        intent.putExtra("number", 123);
+        startActivity(intent);
+        finish();
+    }
+
+    private void checkToSaveName(String name) {
+
+        CheckBox cbSaveName = findViewById(R.id.cbSaveName);
+        if (cbSaveName.isChecked()) {
+            SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
+            prefs.edit().putString("saved_name", name).apply();
+        }
+    }
+}
